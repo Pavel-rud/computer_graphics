14a834a5b16d1a7c9be7b3820750f00d50f54349
Simoffsky <maks-kirsanov26@mail.ru>
2023-03-17 23:10:36 +1000

    added signal model

--- include/Models/Signal.h
+++ include/Models/Signal.h
@@ -2,24 +2,37 @@
 #include <vector>
 #include <time.h>
 
+//TODO: модель времени
 
 class Channel {
+    using container = std::vector<std::pair<double, tm>>;
+    using iterator = container::iterator;
 public:
-    std::vector<double> frequency();
+    container frequency(); 
+    std::string name;
+    std::pair<iterator, iterator> get_window(tm start_time, tm end_time);
 };
 
 
 class Signal{
 public:
-    std::vector<Channel> Channels;
-    uint64_t get_reports_count(); // N
-    uint64_t get_sampling_frequency();
-    tm get_start_time();
-    tm get_end_time();
+    const std::vector<Channel>& get_channels() {return Channels;}
+    uint64_t get_reports_count() {return reports_count;} // N
+    void set_reports_count(uint64_t count) {reports_count = count;}
+
+    uint64_t get_sampling_frequency() {return sampling_frequency;}
+    void set_sampling_frequency(uint64_t count) {sampling_frequency = count;}
+
+    uint64_t get_channel_count() {return channel_count;}
+    void set_channel_count(uint64_t count) {channel_count = count;}
+    
+    tm start_time;
+    tm end_time;
 
 private:
     uint64_t sampling_frequency; // частота дискретизации
-    tm start_time;
-    tm end_time;
+    uint64_t reports_count;
+    uint64_t channel_count;
+    std::vector<Channel> Channels;
 };
 
